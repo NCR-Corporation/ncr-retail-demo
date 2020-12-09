2b23e215692da3b5208e75f1b854d6cef63ba30b
Kevin Guebert <kevin.guebert@ncr.com>
2020-12-09 11:44:37 -0500

    Closes #30: Download Icon briefly large on load

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Head from 'next/head';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import '~/styles/style.css';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Prevent fontawesome from adding its CSS since we did it manually above:
 import { Provider } from 'next-auth/client';
 import { UserStoreProvider } from '~/context/userStore';
 import { UserCartProvider } from '~/context/userCart';
+
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; /* eslint-disable import/first */
 
 export function reportWebVitals(metric) {
   if (metric.label === 'custom') {
