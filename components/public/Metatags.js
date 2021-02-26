9cd7555ed07ef9753e5677040414c06112d27945
Kevin Guebert <kevin.guebert@ncr.com>
2021-02-26 10:20:59 -0500

    Updating metatags url

--- components/public/Metatags.js
+++ components/public/Metatags.js
@@ -3,6 +3,7 @@ export default function Metatags({ children }) {
   const title = 'MART | Sample App Retail Demo';
   const description =
     "A sample retail demo application built on top of NCR's APIs.";
+  const socialImage = `${process.env.REACT_APP_URI}/mart.png`;
   return (
     <div>
       <Head>
@@ -12,21 +13,18 @@ export default function Metatags({ children }) {
 
         <meta itemProp="name" content={title} />
         <meta itemProp="description" content={description} />
-        <meta itemProp="image" content={`/mart.png`} />
+        <meta itemProp="image" content={socialImage} />
 
-        <meta property="og:url" content="https://retaildemo.ncrcloud.com" />
+        <meta property="og:url" content={process.env.REACT_APP_URI} />
         <meta property="og:type" content="website" />
         <meta itemProp="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content={`/mart.png`} />
+        <meta property="og:image" content={socialImage} />
 
         <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          property="twitter:title"
-          content="https://retaildemo.ncrcloud.com"
-        />
+        <meta property="twitter:title" content={process.env.REACT_APP_URI} />
         <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={`/mart.png`} />
+        <meta name="twitter:image" content={socialImage} />
       </Head>
       {children}
     </div>
