5afb4520c89e95410413297e8e7924ffc7fe9f3f
Kevin Guebert <kevin.guebert@ncr.com>
2021-08-20 07:51:17 -0400

    Updating homepage to use `useFetch` hook

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,23 @@
 import { useContext } from 'react';
 import Footer from '~/components/public/Footer';
 import Header from '~/components/public/Header';
-import useHomepage from '~/lib/hooks/useHomepage';
 import { UserStoreContext } from '~/context/userStore';
 import HomeCarousel from '~/components/public/home/HomeCarousel';
 import HomeAboutCards from '~/components/public/home/HomeAboutCards';
 import HomeGroups from '~/components/public/home/HomeGroups';
 import HomeMap from '~/components/public/home/HomeMap';
 import HomeQuote from '~/components/public/home/HomeQuote';
+import { useFetch } from '~/lib/hooks/useFetch';
 
 import { getCategoryNodesForMenu } from '~/lib/category';
 
-function Home({ categories }) {
+function Home({ categories, logs = [] }) {
   const { userStore } = useContext(UserStoreContext);
-  const { data, isLoading, isError } = useHomepage(userStore.id);
+  const { data, isError, isLoading } = useFetch('/api/groups');
+
   return (
     <div className="d-flex flex-column main-container">
-      <Header categories={categories} logs={data && data.logs ? data.logs : []} />
+      <Header categories={categories} logs={data && data.logs ? logs.concat(data.logs) : []} />
       <HomeCarousel />
       <div className="container my-4 flex-grow-1">
         <div>
