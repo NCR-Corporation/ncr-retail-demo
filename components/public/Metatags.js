c86bd2e133116fd2d02d9635b8b3a3e5741c29b6
Benjamin McKinley <bm185264@ncrvoyix.com>
2026-02-04 10:21:59 -0500

    Expose client config variables in Next app

--- components/public/Metatags.js
+++ components/public/Metatags.js
@@ -1,8 +1,11 @@
 import Head from 'next/head';
+import getConfig from "next/config";
+
 export default function Metatags({ children }) {
   const title = 'MART | Sample App Retail Demo';
   const description = "A sample retail demo application built on top of NCR's APIs.";
   const socialImage = `mart.png`;
+  const { publicRuntimeConfig } = getConfig();
   return (
     <div>
       <Head>
@@ -14,7 +17,7 @@ export default function Metatags({ children }) {
         <meta itemProp="description" content={description} />
         <meta itemProp="image" content={socialImage} />
 
-        <meta property="og:url" content={process.env.REACT_APP_URI} />
+        <meta property="og:url" content={publicRuntimeConfig.APP_URI} />
         <meta property="og:type" content="website" />
         <meta itemProp="og:title" content={title} />
         <meta property="og:description" content={description} />
